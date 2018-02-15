13ef7b0e6e29f8f31269038f558f50d918967d95
Jon Liebold <jliebold@outlook.com>
2018-02-15 09:35:16 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Content Pillars
+
+This plugin allows users to create a content page that will serve as a pillar in a cluster of related pieces of content.
+
+It works by creating a custom post type where authors can write the about the overall topic and then will link up to 22 of the latest posts in the category or tag selected. 
---
+++ content-pillars.php
@@ -0,0 +1,10 @@
+/*
+Plugin Name: Content Pillars
+Plugin URI: http://jonliebold.com
+Description: A plugin that allows users to create a content page that will serve as a pillar in a cluster of related pieces of content.
+Version: 1.0
+Author: Jon Liebold
+Author URI: http://jonliebold.com
+Text Domain: jlcpillar
+License: GPLv2 or later
+*/
